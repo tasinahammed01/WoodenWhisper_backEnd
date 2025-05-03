158b3199a125f3439062520354e8ce52a4c95bda
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-05-03 21:18:51 +0600

    added

--- dist/controller/journalsController.js
+++ dist/controller/journalsController.js
@@ -1,11 +1,20 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getJournals = void 0;
 const journalsModules_1 = require("../modules/journalsModules"); // Assuming this is your Mongoose model
 // Function to handle getting all images
-const getJournals = async (_req, res) => {
+const getJournals = (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const journals = await journalsModules_1.Journals.find();
+        const journals = yield journalsModules_1.Journals.find();
         // console.log(journals);
         res.status(200).json(journals);
     }
@@ -13,5 +22,5 @@ const getJournals = async (_req, res) => {
         console.error(error);
         res.status(500).json({ error: "Failed to fetch images" });
     }
-};
+});
 exports.getJournals = getJournals;
